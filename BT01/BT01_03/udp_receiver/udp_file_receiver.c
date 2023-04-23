bcee09491e477a314d297a689a208b4a233b557b
nguyenquangtiep <nguyenquangtiep175@gmail.com>
2023-04-24 02:15:59 +0700

    Done BT01_03

--- BT01/BT01_03/udp_receiver/udp_file_receiver.c
+++ BT01/BT01_03/udp_receiver/udp_file_receiver.c
@@ -7,9 +7,68 @@
 #include <string.h>
 #include <netdb.h>
 
-int main() {
+#define BUFFER_SIZE 1024
 
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Use: %s <Port>\n", argv[0]);
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+
+    int server_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (server_sockfd < 0) {
+        perror("[ERROR] Socket() failed.\n");
+        return 1;
+    }
+
+    struct sockaddr_in server_addr, client_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int e = bind(server_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (e < 0) {
+        perror("[ERROR] Bind() failed.\n");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+
+    socklen_t addr_size = sizeof(client_addr);
+    int n = recvfrom(server_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
+    if (n < 0) {
+        perror("[ERROR] Receiving file name failed.\n");
+        return 1;
+    }
+    printf("[SUCCESS] Received file name.\n");
+
+    char *filename = buffer;
+
+    FILE *file = fopen(filename, "w");
+    if (file < 0) {
+        perror("[ERROR] Opening file failed.\n");
+        return 1;
+    }
+    printf("[SUCCESS] Opened file.\n");
     
+    while (1) {
+        n = recvfrom(server_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
+        if (strcmp(buffer, "END") == 0) {
+            break;
+        }
+        printf("[RECEIVING] Data: %s\n", buffer);
+        fprintf(file, "%s", buffer);
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    printf("[SUCCESS] Data transfer complete.\n");
+    printf("[CLOSING] Closing the server.\n");
+
+    fclose(file);
+    close(server_sockfd);
 
     return 0;
 }
--- BT01/BT01_03/udp_sender/udp_file_sender.c
+++ BT01/BT01_03/udp_sender/udp_file_sender.c
@@ -7,9 +7,61 @@
 #include <string.h>
 #include <netdb.h>
 
-int main() {
+#define BUFFER_SIZE 1024
 
-    
+int main(int argc, char *argv[]) {
+
+    if (argc != 4) {
+        printf("Use: %s <IP Address> <Port> <Filename>\n", argv[0]);
+        return 1;
+    }
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    char *filename = argv[3];
+
+    int server_sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (server_sockfd < 0) {
+        perror("[ERROR] Socket() failed.\n");
+        return 1;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("[ERROR] Opening file failed.\n");
+        return 1;
+    }
+
+    int n = sendto(server_sockfd, filename, sizeof(filename), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (n < 0) {
+        perror("[ERROR] Sending file name failed.\n");
+        return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+
+    while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
+        printf("[SENDING] Data: %s\n", buffer);
+
+        if (sendto(server_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+            perror("[ERROR] Sending data failed.\n");
+            return 1;
+        }
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    strcpy(buffer, "END");
+    sendto(server_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    printf("[SUCCESS] Data transfer complete.\n");
+    printf("[CLOSING] Disconnecting from the server.\n");
+
+    fclose(file);
+    close(server_sockfd);
 
     return 0;
 }
